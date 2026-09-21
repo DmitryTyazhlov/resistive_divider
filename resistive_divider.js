e84188327214bea0750a1c31b6c4dd6fa913e40b
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2026-09-21 18:41:44 +0300

    up

--- resistive_divider.js
+++ resistive_divider.js
@@ -8,10 +8,18 @@ const rl = readline.createInterface({ input, output });
 
 let path = "./"
 
-const selectionResistors = (vOut, vRef) => {
+const selectionResistors = (db, vOut, vRef, precision) => {
   let coefficient = vOut / vRef - 1
   fs.readdir(path, function (err, items) {
-    let file = './db_resistors.txt';
+    let file = './db_korolab.txt'
+    switch (db) {
+      case "1":
+        file = './db_korolab.txt';
+        break;
+      case "2":
+        file = './db_resistors.txt';
+        break;
+    }
     readFile(file, 'utf-8', function (err, contents) {
       if (err) {
         console.log(err);
@@ -21,21 +29,25 @@ const selectionResistors = (vOut, vRef) => {
       let numberValuel = [];
       stringValues.forEach(function (item, i, arr) {
         if (item.includes("mΩ")) {
-          numberValuel.push(parseFloat(item, 10)/1000);
+          numberValuel.push(parseFloat(item, 10) / 1000);
         } else if (item.includes("kΩ")) {
-          numberValuel.push(parseFloat(item, 10)*1000);
+          numberValuel.push(parseFloat(item, 10) * 1000);
         } else if (item.includes("MΩ")) {
-          numberValuel.push(parseFloat(item, 10)*1000000);
+          numberValuel.push(parseFloat(item, 10) * 1000000);
         } else if (item.includes("Ω")) {
           numberValuel.push(parseFloat(item, 10))
         }
       })
       numberValuel.forEach(function (item, i, arr) {
         let r1 = item
-        let r2 = item * coefficient
-        numberValuel.forEach(function (item, j, arr) {
-          if (r2 === item) {
-            console.log("r1: " + stringValues[i] + " r2: " + stringValues[j] + '\n')
+        numberValuel.forEach(function (item2, j, arr) {
+          let r2 = item2
+          let result_v = vOut / (r1 + r2) * r2
+          let out_v = vRef * (r1 + r2) / r2
+          let minV = result_v - (result_v * precision / 100)
+          let maxV = result_v + (result_v * precision / 100)
+          if ((minV <= vRef) && (vRef <= maxV)) {
+            console.log("r1: " + stringValues[j] + " r2: " + stringValues[i] + " Res:" + result_v.toFixed(5) + "v" + " Out_V:" + out_v.toFixed(5) + '\n')
           }
         })
       })
@@ -47,6 +59,10 @@ const selectionResistors = (vOut, vRef) => {
 // main
 (async function () {
 
+  let db = await new Promise(resolve => {
+    rl.question("База данных (1-королаб, 2-глобальная): ", resolve)
+  })
+
   let outputVoltage = await new Promise(resolve => {
     rl.question("Выходное напряжение: ", resolve)
   })
@@ -55,8 +71,11 @@ const selectionResistors = (vOut, vRef) => {
     rl.question("Напряжение обратной связи: ", resolve)
   })
 
-  selectionResistors (outputVoltage, refVoltageFeedBack)
-  
+  let precision = await new Promise(resolve => {
+    rl.question("Допуск %: ", resolve)
+  })
+  selectionResistors(db, outputVoltage, refVoltageFeedBack, precision)
+
   await new Promise(resolve => {
     rl.question("", resolve)
   })
